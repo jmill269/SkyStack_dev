2cc22c63f2c43eb2154ac9ea39996ace2f1e4efe
jmill269 <jmill2698@gmail.com>
2018-03-25 18:40:26 -0400

    testing on Pi

--- src/calibrate.cpp
+++ src/calibrate.cpp
@@ -31,6 +31,31 @@ void createArucoMarkers() {
     }
 }
 
+void createKnownBoardPos(Size boardSize, float sqEdgeLen, vector<Point3f>& corners) {
+    for (int i = 0; i < boardSize.height; i++) {
+        for (int j = 0; j < boardSize.width; j++) {
+            corners.push_back(Point3f(j*sqEdgeLen, i*sqEdgeLen, 0.0f));
+        }
+    }
+}
+
+void getBoardCorners(vector<Mat> imgs, vector<vector<Point2f>>& allFoundCorners, bool showResults = false) {
+    for (vector<Mat<::iterator it = imgs.begin(); it != imgs.end; it++) {
+        vector<Point2f> pointBuf;
+        bool found = findChessboardCorners(*it, Size(boardDimensions), pointBuf, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_NORMALIZE_IMAGE);
+
+        if (found) {
+            allFoundCorners.push_back(pointBuf);
+        }
+        if (showResults) {
+            drawChessboardCorners(*it, Size(boardDimensions), pointbuf, found);
+            imshow("Looking for Corners", *it);
+            waitKey(0);
+        }
+    }
+}
+
+
 int main(int agrgv, char** argc) {
     createArucoMarkers();
 
