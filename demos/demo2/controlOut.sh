027f6d1647e6e03020f7aa44f77fc26d0a5afd4a
Chris Scherz <scherz.chris@gmail.com>
2018-08-30 12:10:20 -0400

    created new poseEstimation src to play around with it

--- demos/demo2/controlOut.sh
+++ demos/demo2/controlOut.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
+ #File should be kept in same directory as this file.
 
-python2 control_gimbal3.py & #File should be kept in same directory as this file.
+programStart() {
 
-echo "Started Gimbal Control Program"
+	echo "Started Gimbal Control Program" >&2
 
-STOP='False'
+	STOP='False'
 
-while [[ $STOP != 'True' ]]
-do
+	while [[ $STOP != 'True' ]]
+		do
 
-	echo "Start Controlling Gimbal"
-	echo "1 - Low Position"
-	echo "2 - Medium Position"
-	echo "3 - High Position"
-	echo "0 - Stop Program"
-	read -p 'Please Enter Your Command' COMMAND
+		echo "Start Controlling Gimbal" >&2
+		echo "1 - Low Position" >&2
+		echo "2 - Medium Position" >&2
+		echo "3 - High Position">&2
+		echo "0 - Stop Program" >&2
+		read COMMAND
 
-	if [ $COMMAND -eq 0 ]
-	then
-		$STOP=True
-	fi
-	if [ $COMMAND -gt 4 ]
-	then
-		echo "Command Input is Invalid."
-		echo "Please enter a value between 0 and 3"
-	fi
+		if [[ $COMMAND -eq 0 ]]
+		then
+			$STOP=True
+		fi
+		if [[ $COMMAND -gt 4 ]]
+		then
+			echo "Command Input is Invalid." >&2
+			echo "Please enter a value between 0 and 3" >&2
+		else
+			echo $COMMAND >&1 # This should be the only stdout.
+		fi
 
-	echo $COMMAND | control_gimbal3.py
-done
+	done
+}
 
+programStart | ./control_gimbal4.py
