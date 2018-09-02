2b49a71f694c8efa057c01ec6106794dfe914bc8
Christopher Scherz <scherz.chris@gmail.com>
2018-09-02 23:09:42 +0000

    Figured out how to parse data properly but ran into new problem. ./cal_faster needs to be run on the drone and control_gimbal.py - as it is - needs to run on GCS. Perhaps it is possible to run dronekit programs from the drone itself

--- demos/demo3/control_gimbal4.py
+++ demos/demo3/control_gimbal4.py
@@ -18,25 +18,13 @@ print ("Waiting on Input")
 
 for line in sys.stdin:
 
-while test == 1:
-	line = input('1 - LOW | 2 - MEDIUM | 3 - HIGH | 4 - STOP')
-	if line == 1:
-		#Tell the gimbal to point straight down.
+	newLine=line.replace("[","")
+	newline=newline.replace("]","")
+	values = line.split(',')
+	ytrans = values[1]
+	if ytrans > #NEEDS MORE WORK
+	vehicle.gimbal.rotate(90,0,0)
 
-		vehicle.gimbal.rotate(90,0,0)
-		print ("Gimbal should be looking down")
-	if line == 2:
-		#Tell gimbal to look forward
-		vehicle.gimbal.rotate(0,0,0)
-		print ("Gimbal should be looking forward")
-	if line == 3:
-		#Tell gimbal to look up
-		vehicle.gimbal.rotate(-90,0,0)
-		print ("Gimbal should be looking up")
-	if line == 4:
-		vehicle.close()
-		print ("Vehicle Connection Closed")
-		test = 0
 
  #close vehicle for proper shutdown.
 #vehicle.close()
